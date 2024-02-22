9864fbf1cbed92482869a8aa575a46d5970def3f
emirtotic <emirtotic@gmail.com>
2024-02-22 10:43:04 +0100

    minikube implementation - bootstrap

--- customer/src/main/java/com/emirtotic/customer/CustomerApplication.java
+++ customer/src/main/java/com/emirtotic/customer/CustomerApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -16,6 +18,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients(
         basePackages = "com.emirtotic.clients"
 )
+@PropertySources({
+        @PropertySource("classpath:client-${spring.profiles.active}.properties")
+})
 public class CustomerApplication {
 
     public static void main(String[] args) {
--- notification/src/main/java/com/emirtotic/notification/NotificationApplication.java
+++ notification/src/main/java/com/emirtotic/notification/NotificationApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @EnableEurekaClient
 @SpringBootApplication(
@@ -14,6 +16,9 @@ import org.springframework.context.annotation.Bean;
                 "com.emirtotic.amqp"
         }
 )
+@PropertySources({
+        @PropertySource("classpath:client-${spring.profiles.active}.properties")
+})
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
